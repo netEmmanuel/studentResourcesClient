efe3c48b438d05c683397680420b40921b975a29
d.dammylarry@gmail.com <d.dammylarry@gmail.com>
2017-11-10 13:48:34 +0000

    make it better

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(path.join(__dirname + '/dist' )));
 
 app.listen(process.env.PORT || 8080 );
 
 // pathlocationStrategy
 
-app.get('/', function(req, res, next) {
-   res.sendFile(path.join(__dirname + '/index.html' ));
+app.get('*', function(req, res, next) {
+   res.sendFile(path.join(__dirname + 'dist/index.html' ));
   });
     console.log("console listening");
